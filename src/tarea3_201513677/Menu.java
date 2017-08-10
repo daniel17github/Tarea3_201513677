6af24ba455cbdb06d74f8b189eb6d773bd547a25
Dany <Dany@DESKTOP-AF2J7VE>
2017-08-09 20:22:43 -0600

    Commit3 solo un ultimo arreglo

--- src/tarea3_201513677/Menu.java
+++ src/tarea3_201513677/Menu.java
@@ -3,14 +3,14 @@ package tarea3_201513677;
   
     import java.util.Scanner;
 
-public  class Menu {
-    public int opciones;
+public final  class Menu {
+    public int Opciones;
     public String[] Usuarios;
-    public String NUsuario;
+    public String NoUsuario;
     
     public Menu(){
-        opciones = 0;
-        NUsuario = "";  
+        Opciones = 0;
+        NoUsuario = "";  
         Usuarios = new String[5];
         
         do {
@@ -18,15 +18,15 @@ public  class Menu {
             System.out.println("         201513677          ");
             System.out.println(" 1. Usuarios                ");
             System.out.println(" 2. Palabras Palíndromas    ");
-            System.out.println(" 3. Salir                   ");            
+            System.out.println(" 3. Salir                   ");    
             Scanner leer = new Scanner(System.in);
-            opciones = leer.nextInt();
-            switch(opciones){
+            Opciones = leer.nextInt();
+            switch(Opciones){
                 case 1:
                     MenuUs();
                     break;
                 case 2:
-                    PalbrasPalindromas();
+                    PalabrasPalindromas();
                     break;
                 case 3:
                     break;
@@ -35,7 +35,7 @@ public  class Menu {
                     break;
 
             }
-        } while (opciones != 3);
+        } while (Opciones != 3);
     }
     private void MenuUs(){
         System.out.println("            Menu de Usuarios             ");
@@ -43,11 +43,10 @@ public  class Menu {
         System.out.println(" 2. Mostrar todos los Usuarios           ");
         System.out.println(" 3. Mostrar un Usuario Personalizado     ");
         System.out.println(" 4. Menú Principal                       ");
-        System.out.println(" 5. Salir                                ");
         Scanner opcUs = new Scanner(System.in);
-        opciones = opcUs.nextInt();
+        Opciones = opcUs.nextInt();
         do {
-            switch(opciones){
+            switch(Opciones){
                 case 1:
                     IngresarU();
                     break;
@@ -59,45 +58,48 @@ public  class Menu {
                     break;
                 case 4:
                     break;
-                case 5:
-                    break;
                 default:
                     System.out.println("Opcion Invalida");
                     break;
             }
             
-        } while (opciones != 4);
+        } while (Opciones != 4);
     }
 
     public void IngresarU(){
         for (int i = 0; i < Usuarios.length; i++) {
-            System.out.println("Ingrese un nuevo usuario: ");
+            System.out.println("Ingrese un usuario: ");
             Scanner us = new Scanner(System.in);
-            NUsuario = us.nextLine();
-            Usuarios[i]= NUsuario;
+            NoUsuario = us.nextLine();
+            Usuarios[i]= NoUsuario;
                 
             }
         MenuUs();
     }
+    
+    
     public void MostrarU(){
+         System.out.println("         Mostrar Usuarios           ");
         for (int i = 0; i < Usuarios.length; i++) {
-            System.out.println((i+1)+". "+Usuarios[i]);            
+           System.out.println((i+1)+".------------> "+Usuarios[i]);            
         }
         MenuUs();
     }
+    
+    
     public void UsuarioP(){
-  
-        System.out.println("Ingrese un Usuario: ");
+        System.out.println("     Mostrar un Usuario Personalizado     ");
+        System.out.println("              Ingrese Usuario:            ");
         Scanner usper = new Scanner(System.in);
-        NUsuario = usper.nextLine();
+        NoUsuario = usper.nextLine();
         for (int i = 0; i < Usuarios.length; i++) {
             
-            if (Usuarios[i].equals(NUsuario)) {
-                System.out.println("Usuario");
+            if (Usuarios[i].equals(NoUsuario)) {
+                System.out.println("          Usuario            ");
                 System.out.println(Usuarios[i]);
 
-            } else {
-                System.err.println("!!Error!! no existes el Usuario");
+            } else if( i+1==5) {
+                System.err.println("!!Error!! no existe ningun usuario con esa coincidencia");
                 
             }    
         }
@@ -105,15 +107,15 @@ public  class Menu {
         
         MenuUs();
     }
-    public void PalbrasPalindromas(){
+        
+    public void PalabrasPalindromas(){
     
         String palabra;
-            
         Scanner sc = new Scanner(System.in);
         System.out.println("*************Palabras Palindromas***************");
         System.out.println("              Ingrese palabra                  ");
         palabra = sc.nextLine();
-        System.out.println("");
+        
         if(comparacionpalindroma(palabra)){
             System.out.println(" ''''''''' SI ES PALINDROMA '''''''''   ");   
         }else{
@@ -130,7 +132,7 @@ public  class Menu {
             if(!(palab.substring(i,i + 1).equals(" ")
                     ||palab.substring(i,i + 1).equals(" ,"))){
               aux +=palab.substring(i,i + 1);
-            
+              
             }
         }
         
